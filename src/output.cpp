a0890b3c793136639bb1dc4090454ed2960e6d2d
kidibra <kidibra2020@gmail.com>
2023-03-17 08:57:20 +0300

    feat: c++ output

---
+++ src/output.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // use '\n' to print a new line
+    cout << "Hello World! \n \n";
+    // use 'endl' to print multiple lines
+    cout << "I am a C++ program" << endl;
+
+    // use '\t' to print a tab
+    cout << "I am learning \t C++";
+
+    // use '\\' to print a backslash
+    cout << "I am learning \\ C++";
+
+    // use '\"' to print a double quote
+    cout << "I am learning \" C++";
+    return 0;
+}
