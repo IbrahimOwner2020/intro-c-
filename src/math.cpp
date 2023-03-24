11e6caef07fbafde926e469ac096af45724ea6b5
kidibra <kidibra2020@gmail.com>
2023-03-24 10:06:58 +0300

    math in c++

---
+++ src/math.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    cout << "Math stuff has started" << endl;
+    cout << max(7, 8) << endl;
+    cout << min(12, 9) << endl;
+
+    // Other math operation needs to include the '<cmath>' header;
+    cout << sqrt(64) << endl;
+    cout << round(3.14) << endl;
+    cout << log(2) << endl;
+
+    // Other math popular functions are available in :
+    // https://www.w3schools.com/cpp/cpp_math.asp
+
+    return 0;
+}
